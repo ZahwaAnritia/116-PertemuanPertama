a98af4aecbd6b45d48ffa30ad2bc771d9baa8999
ZahwaAnritia <anritia15@gmail.com>
2024-02-26 11:30:49 +0700

    display masukkan jejari

--- PertemuanPertama/PertemuanPertama.cpp
+++ PertemuanPertama/PertemuanPertama.cpp
@@ -1,14 +1,20 @@
 ﻿
-
 #include <iostream>
 using namespace std;
 
 
 int main()
 {
-    cout << "Hello World!" << endl;
-    cout << "Hello";
-    
+    //begin
+        // numeric nJejari, nLuas
+        // display 'Masukkan jejari= '
+        // accept nJejari
+        // compute nLuas = 3.14 * r * r
+        //display 'Luasnya ' + Luas
+    //end
+
+    double nJejari, nLuas;
+    cout << "Masukkan Jejari = ";
     system("pause");
 }
 
